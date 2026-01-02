e7cb3f7ef098ec7149aaea5a4d691a9470b01d94
chamikathereal <118957556+chamikathereal@users.noreply.github.com>
2026-01-03 00:15:05 +0530

    api implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [city, setCity] = useState();
+
+  const testApi = async ()=>{
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=colombo&appid=472af64eabc5a2676ee87c5845a83386`);
+    if(!response.ok){
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    console.log(data);
+
+  }
+
+  useEffect(()=>{
+    testApi();
+  },[])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
